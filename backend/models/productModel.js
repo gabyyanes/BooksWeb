a9650e172781a562db1c336f696c4a7cfc6792d6
bwill210 <bwill210@fiu.edu>
2021-04-04 15:05:15 -0400

    Updated product attributes
    Deleted _id and added author, genre, and release_date

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -4,10 +4,13 @@ const productSchema = new mongoose.Schema(
   {
     name: { type: String, required: true, unique: true },
     image: { type: String, required: true },
+    author: { type: String, required: true },
     description: { type: String, required: true },
     price: { type: Number, required: true },
     countInStock: { type: Number, required: true },
     rating: { type: Number, required: true },
+    genre: { type: String, required: true },
+    release_date: { type: Date, required: false },
   },
   {
     timestamps: true,
